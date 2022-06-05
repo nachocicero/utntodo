d156138f21e5be36b2d5ca3307ca695128d4a287
nachocicero <nachocicero@gmail.com>
2022-06-05 18:16:38 -0300

    uhff

---
+++ clase08/js/index.js
@@ -0,0 +1,17 @@
+console.log ('hola , soy el archivo javascript');
+
+var nombre = 'nacho';
+
+console.log(nombre);
+
+let ciudad = 'rosario';
+
+console.log(ciudad);
+
+console.log('mi nombre es : ' +nombre+ " y mi ciudad es : " +ciudad);
+
+console.log(`mi nombre es : ${nombre} y mi ciudad es : ${ciudad}`);
+
+const DNI = '12345678'; 
+console.log (`mi dni es :  ${DNI}  y no se puede cambiar el valor de la constante`);
+
